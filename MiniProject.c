05e48f4da97446ac93d014c6691fb3b190632836
NaamyFatimaZahra <naamyfatimazahra@gmail.com>
2024-09-03 18:13:15 +0200

    changes in mini projects

--- MiniProject.c
+++ MiniProject.c
@@ -3,13 +3,30 @@
 
 //fonction d'ajoute.
 void Ajouter(){
+    int c = 0;
     char Titre[60][15];
     printf("entrer le titre:");
-   for(int i=0;i<60;i++){
-    gets(Titre[i]);
-   }
-
+   //for(int i=0;i<60;i++){
+    gets(Titre[c]);
+    //break;
+   //}
+    char auteur[60][15];
+    printf("entrer l'auteur:");
+   //for(int i=0;i<60;i++){
+    gets(auteur[c]);
+    //break;
+   //}
+   c++;
+char all[60][15][2];
+ for(int i=0;i<60;i++){
+ for(int j=0;j<15;j++){
+    all[i][j][0]=auteur[i][j];
+    all[i][j][1]=Titre[i][j];
+ }
 }
+puts(all);
+}
+
 
 int main(){
 
