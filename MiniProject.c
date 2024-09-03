bf3bf4ee10cc47453f62d7333ce38254eb1767a5
NaamyFatimaZahra <naamyfatimazahra@gmail.com>
2024-09-03 17:07:15 +0200

    Mini Project

--- MiniProject.c
+++ MiniProject.c
@@ -2,15 +2,14 @@
 #include<stdio.h>
 
 //fonction d'ajoute.
-void Ajouter(char Titre[]){
-
+void Ajouter(){
+    char Titre[10];
     gets(Titre);
     puts(Titre);
 
 }
 
 int main(){
-    char Titre[10];
-Ajouter(Titre);
+Ajouter();
 
 }
